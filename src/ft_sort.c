b76f76829ae741f9a15c8558dd79256586e14962
maximart <maxime.martinez96@hotmail.fr>
2025-01-06 13:58:57 +0100

    Refactor(logic): change the condition "if" by "else if" for a better visual

--- src/ft_sort.c
+++ src/ft_sort.c
@@ -20,13 +20,9 @@ void	ft_sort(t_stack **s_a, t_stack **s_b, t_data *data)
 		{
 			ft_pb(s_a, s_b);
 			ft_rb(s_b);
-			continue ;
 		}
-		if ((*s_a)->value <= data->q2 && (*s_a)->value >= data->med)
-		{
+		else if ((*s_a)->value <= data->q2 && (*s_a)->value >= data->med)
 			ft_pb(s_a, s_b);
-			continue ;
-		}
 		else
 			ft_ra(s_a);
 	}
